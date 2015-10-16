99a3c811919e91ca30b917c4a99df19055998114
Zareen Khayrattee <zareenk94@yahoo.com>
2015-10-16 17:05:09 -0500

    no message

--- 02 Data Wrangling/plot1_inner.R
+++ 02 Data Wrangling/plot1_inner.R
@@ -17,8 +17,9 @@ join_df <- inner_join(trauma_df, premature_df, by = c("COUNTRY","YEAR")) %>% arr
 View(join_df)
 summary(join_df)
 
-join_df %>% mutate(Percent_trauma = TRAUMA_0_TO_27_DAYS / TRAUMA_TOTAL, Percent_prem = PREM_0_TO_27_DAYS / PREM_TOTAL) %>% filter(Percent_trauma>=.5 && Percent_prem>=.5) %>% ggplot(aes(x=Percent_prem, y=Percent_trauma, color=YEAR)) + geom_point() + labs(x="PERCENTAGE OF PREMATURITY-CAUSED DEATHS", y="PERCENTAGE OF TRAUMA-CAUSED DEATHS") + labs(title="DEATHS IN BABIES 0 TO 27 DAYS OLD")
 
-join_df %>% mutate(Percent_trauma = TRAUMA_0_TO_27_DAYS / TRAUMA_TOTAL, Percent_prem = PREM_0_TO_27_DAYS / PREM_TOTAL) %>% filter(YEAR==2013) %>% ggplot(aes(x=Percent_prem, y=Percent_trauma, color=COUNTRY)) + geom_point() + labs(x="PERCENTAGE OF PREMATURITY-CAUSED DEATHS", y="PERCENTAGE OF TRAUMA-CAUSED DEATHS") + labs(title="DEATHS IN BABIES 0 TO 27 DAYS OLD IN 2013")
+join_df %>% mutate(Percent_trauma = TRAUMA_0_TO_27_DAYS / TRAUMA_TOTAL, Percent_prem = PREM_0_TO_27_DAYS / PREM_TOTAL) %>% filter(TRAUMA_TOTAL+PREM_TOTAL > 19000) %>% ggplot(aes(x=Percent_prem, y=Percent_trauma, color=COUNTRY)) + geom_point() + facet_wrap(~YEAR) + labs(x="PERCENTAGE OF PREMATURITY-CAUSED DEATHS", y="PERCENTAGE OF TRAUMA-CAUSED DEATHS") + labs(title="DEATHS IN BABIES 0 TO 27 DAYS OLD")
 
-join_df %>% mutate(Percent_deaths_less_than_one_month = cume_dist(DEATHS_LESS_THAN_1_MONTH)) %>% filter( Percent_deaths_less_than_one_month <=.10 | Percent_deaths_less_than_one_month >= .90) %>% ggplot(aes(x=Percent_deaths_less_than_one_month, y=DEATHS_1_TO_59_MONTHS, color=COUNTRY)) + geom_point() + labs(x="CUMULATIVE DISTRIBUTION OF DEATHS", y="TOTAL DEATHS IN 1 TO 59 MONTHS") + labs(title="DEATHS IN BABIES LESS THAN ONE MONTH OLD")
+join_df %>% mutate(Percent_trauma = TRAUMA_0_TO_27_DAYS / TRAUMA_TOTAL, Percent_prem = PREM_0_TO_27_DAYS / PREM_TOTAL) %>% filter(TRAUMA_TOTAL+PREM_TOTAL > 19000) %>% ggplot(aes(x=Percent_prem, y=Percent_trauma, color=YEAR)) + geom_point() + facet_wrap(~COUNTRY) + labs(x="PERCENTAGE OF PREMATURITY-CAUSED DEATHS", y="PERCENTAGE OF TRAUMA-CAUSED DEATHS") + labs(title="DEATHS IN BABIES 0 TO 27 DAYS OLD")
+
+
