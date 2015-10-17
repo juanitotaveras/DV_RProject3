4143b460757289e14c5ebb2d560577e18f83753d
Kayne Khoury <kaynekhoury1@gmail.com>
2015-10-16 19:42:54 -0500

    .rmd should be working now

--- 02 Data Wrangling/Blending_P3.R
+++ 02 Data Wrangling/Blending_P3.R
@@ -1,9 +1,3 @@
-require("jsonlite")
-require("RCurl")
-require("dplyr")
-require("tidyr")
-require("ggplot2")
-require("extrafont")
 
 # Change the USER and PASS below to be your UTEid
 premature_df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from premature"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_kdk745', PASS='orcl_kdk745', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
